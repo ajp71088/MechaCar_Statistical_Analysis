6aa0c900e8f6b1ee295f6f454cd5230711d6b717
Alexander Pendergrass <apendergrass88@gmail.com>
2022-09-20 11:25:25 -0400

    Deliverable 3 code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,12 @@ lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean
                                                                             Median=median(PSI),
                                                                             Variance=var(PSI),
                                                                             SD=sd(PSI))
+
+#Deliverable 3
+
+t.test(SuspensionCoil$PSI,mu=1500) #t.test to find if PSI across all lots is statistically different from the population mean of 1500
+
+#Three more t.tests to find if PSI for each lot is statistically different from the population mean of 1500
+t.test(subset(SuspensionCoil,Manufacturing_Lot=='Lot1')$PSI,mu=1500)
+t.test(subset(SuspensionCoil,Manufacturing_Lot=='Lot2')$PSI,mu=1500)
+t.test(subset(SuspensionCoil,Manufacturing_Lot=='Lot3')$PSI,mu=1500)
