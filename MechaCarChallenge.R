74523a7b660f76c08e4549f8687f2b02e1b831a7
Alexander Pendergrass <apendergrass88@gmail.com>
2022-09-20 10:51:35 -0400

    Deliverable 2 code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+#Deliverable 1
+
 library(dplyr)
 
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
@@ -5,3 +7,19 @@ MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,MechaCar) #create linear model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,MechaCar)) #summarize linear model
+
+#Deliverable 2
+
+SuspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Create a summary dataframe
+total_summary <- SuspensionCoil %>% summarize(Mean=mean(PSI),
+                                              Median=median(PSI),
+                                              Variance=var(PSI),
+                                              SD=sd(PSI))
+
+#Create a lot summary dataframe
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                            Median=median(PSI),
+                                                                            Variance=var(PSI),
+                                                                            SD=sd(PSI))
